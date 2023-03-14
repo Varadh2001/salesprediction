424a96ce1e15f8930a9b689c6b7da4419fdd1bb2
Varadh2001 <126092305+Varadh2001@users.noreply.github.com>
2023-03-14 07:30:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,6 @@ import matplotlib.pyplot as plt
 import base64
 from sklearn.model_selection import train_test_split
 
-
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
@@ -22,35 +21,48 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-
 add_bg_from_local('ss.jpg')  
 
+# Load the dataset into a pandas DataFrame
 df = pd.read_csv('train.csv')
-df.columns = df.columns.map(str) # convert column names to string
 
-X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item', 'day']], df['sales'], test_size=0.2, random_state=42)
+# Convert the date column to a datetime object
+df['date'] = pd.to_datetime(df['date'])
+
+# Create binary columns for each day of the week
+day_of_week_columns = pd.get_dummies(df['date'].dt.day_name())
+df = pd.concat([df, day_of_week_columns], axis=1)
+
+# Split the dataset into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']], df['sales'], test_size=0.2, random_state=42)
+
+# Train a decision tree regression model on the training set
 model = DecisionTreeRegressor()
 model.fit(X_train, y_train)
 
+# Calculate the accuracy of the model on the testing set
 accuracy = model.score(X_test, y_test)
-print("model used:",model)
+print("model used:", model)
 print("r2:", accuracy)
-day = 'Monday' # Example date
-store = 1 # Example store number
-item = 1 # Example item number
-prediction = model.predict([[store, item, day]])
-print("Prediction:", prediction)
 
-# Create the input form
+# Define the input form
 st.write("# Sales Prediction App")
-day =st.selectbox(
-    'select a day',
+date =st.selectbox(
+    'Select a day',
     ('Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'))
 store = st.number_input("Enter the store number", min_value=1, max_value=10)
 item = st.number_input("Enter the item number", min_value=1, max_value=50)
 
-# Make the prediction
+# Convert the input date to a binary column
+day_column = pd.DataFrame(columns=['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday'])
+day_column[date] = 1
+
+# Make a prediction using the input values
+input_values = pd.concat([pd.DataFrame({'store': [store], 'item': [item]}), day_column], axis=1)
+prediction = model.predict(input_values)
+
+# Display the prediction to the user
 if st.button("Predict"):
-    prediction = model.predict([[store, item, day]])
-    st.write("The predicted sales for {} at store {} for item {} is {}".format(day, store, item, prediction))
+    st.write("The predicted sales for {} at store {} for item {} is {}".format(date, store, item, prediction))
+
 
