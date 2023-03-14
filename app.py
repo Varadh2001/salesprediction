82bdcea67a6655349df998a15d6ffaaf60a10c08
Varadh2001 <126092305+Varadh2001@users.noreply.github.com>
2023-03-14 07:51:04 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,8 @@ import matplotlib.pyplot as plt
 import base64
 from sklearn.model_selection import train_test_split
 
+
+
 def add_bg_from_local(image_file):
     with open(image_file, "rb") as image_file:
         encoded_string = base64.b64encode(image_file.read())
@@ -21,28 +23,33 @@ def add_bg_from_local(image_file):
     """,
     unsafe_allow_html=True
     )
-
 add_bg_from_local('ss.jpg')  
 
+ 
 df = pd.read_csv('train.csv')
+df['date'] = pd.to_datetime(df['date'])
 
-X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item', 'day']], df['sales'], test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(df[['store', 'item']], df['sales'], test_size=0.2, random_state=42)
 model = DecisionTreeRegressor()
 model.fit(X_train, y_train)
 
 accuracy = model.score(X_test, y_test)
 print("model used:",model)
 print("r2:", accuracy)
-
+date = '2023-03-12' # Example date
+store = 1 # Example store number
+item = 1 # Example item number
+prediction = model.predict([[store, item]])
+print("Prediction:", prediction)
 import streamlit as st
 
 # Create the input form
 st.write("# Sales Prediction App")
-day = st.text_input("Enter the day (1-365)")
+date = st.date_input("Select the date")
 store = st.number_input("Enter the store number", min_value=1, max_value=10)
 item = st.number_input("Enter the item number", min_value=1, max_value=50)
 
 # Make the prediction
 if st.button("Predict"):
-    prediction = model.predict([[store, item, int(day)]])
-    st.write("The predicted sales for day {} at store {} for item {} is {}".format(day, store, item, prediction))
+    prediction = model.predict([[store, item]])
+    st.write("The predicted sales for {} at store {} for item {} is {}".format(date, store, item, prediction)
